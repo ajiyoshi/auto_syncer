2d76136f20dfbab3308442af46a1f615d6a166b9
ajiyoshi-vg <Yoichi_Sudo@voyagegroup.com>
2015-02-07 18:23:21 +0900

    initial

---
+++ src/auto_syncer_app.erl
@@ -0,0 +1,17 @@
+-module(auto_syncer_app).
+
+-behaviour(application).
+
+%% Application callbacks
+-export([start/2, stop/1]).
+
+%% ===================================================================
+%% Application callbacks
+%% ===================================================================
+
+start(_StartType, _StartArgs) ->
+    sync:go(),
+    auto_syncer_sup:start_link().
+
+stop(_State) ->
+    ok.
